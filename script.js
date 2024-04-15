c19c1fd4484da2f0534d7dfcc89705885c522e42
ELIZABETE APEINE <apeine.elizabete@gimnazija.local>
2024-04-15 15:40:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert(1)
+//console.log(1)
+function sutitZinu()
+{
+    console.log('sutitZinu()darbojas');
+    let zina= document.querySelector(zina).value;
+    let zinas= document.querySelction('cataZina');
+    zinas.inertHTML = zina.innerHTML+'<br>'zina.value;
+}
