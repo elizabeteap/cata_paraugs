376748c8f24f3fcccb430569688be4511aaeca25
ELIZABETE APEINE <apeine.elizabete@gimnazija.local>
2024-04-22 15:30:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 //console.log(1)
 let zina= document.querySelector('.zina');
-let zinas= document.querySelction('.cataZina');
+let zinas= document.querySelector('.cataZina');
 
 function sutitZinu()
 {
     console.log('sutitZinu()darbojas');
     zinas.inertHTML = zina.innerHTML+'<br>'+zinas.value;
+}
+function ieladetChataZinas()
+{
+    let datiNoServera = await fetch('ChataZinas.txt');
+    let dati = await datiNoServera.text;
 }
